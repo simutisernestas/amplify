9167600a8e6c565f84e4b7cb1bbfe006ff645826
Ernestas <simutisernestas@gmail.com>
2020-12-10 00:36:40 +0200

    handle large images

--- lab.py
+++ lab.py
@@ -12,6 +12,7 @@ eval.get_args()
 import glob
 from pathlib import Path
 Path("results").mkdir(exist_ok=True)
+import imutils
 
 print("Loading trt execution, this may take a while... ")
 sess = rt.InferenceSession("yolact.onnx")
@@ -26,11 +27,14 @@ priors_name = sess.get_outputs()[3].name
 proto_name = sess.get_outputs()[4].name
 
 def inference(img_path, show=False):
-    frame = torch.Tensor(cv2.imread(img_path)).cuda().float()
+    # if too big to fit on GPU
+    img = cv2.imread(img_path)
+    img = imutils.resize(img, width=1024)
+    frame = torch.Tensor(img).cuda().float()
     batch = FastBaseTransform()(frame.unsqueeze(0))
 
     start = time.time()
-    for i in range(10):
+    for i in range(1):
         # inference benchmark
         pred_onx = sess.run([loc_name, conf_name, mask_name, priors_name, proto_name], {input_name: batch.cpu().detach().numpy()})
         detection = layers.Detect(81, bkg_label=0, top_k=200, conf_thresh=0.05, nms_thresh=0.5)
